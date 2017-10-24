09bcf00dc3d5b71722398e78b3884a9e524d3006
Yousuf Qutubuddin <usuf52@gmail.com>
2017-10-24 20:22:15 +0500

    clear model after add rider

--- src/app/add-rider/add-rider.ts
+++ src/app/add-rider/add-rider.ts
@@ -22,6 +22,16 @@ export class AddRiderComponent {
         e.preventDefault();
         this.model.location = [this.model.lat, this.model.lng];
         console.log(this.model);
-        this.fbService.addRider(this.model);
+        this.fbService.addRider({ ...this.model });
+        this.clearModel();
+    }
+
+    clearModel() {
+        this.model = {
+            name: null,
+            lat: null,
+            lng: null,
+            location: null
+        };
     }
 }
